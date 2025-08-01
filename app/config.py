2e4d9fbb1ab0fc275758c88003f90baa991f6f1c
Peiromadeiro <86718443+Peiromadeiro@users.noreply.github.com>
2025-08-01 11:49:32 +0200

    Update config.py
    eu-west-1

--- app/config.py
+++ app/config.py
@@ -3,4 +3,4 @@ import os
 ACCESS_KEY = os.getenv("AMAZON_ACCESS_KEY")
 SECRET_KEY = os.getenv("AMAZON_SECRET_KEY")
 ASSOCIATE_TAG = os.getenv("AMAZON_ASSOCIATE_TAG")
-REGION = "ES"
+REGION = "eu-west-1"
