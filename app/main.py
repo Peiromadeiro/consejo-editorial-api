2ebe8b3fbf3fdbd9a85258a1029a6f97717d208b
Peiromadeiro <86718443+Peiromadeiro@users.noreply.github.com>
2025-08-01 08:37:10 +0200

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -1,10 +1,20 @@
-from dotenv import load_dotenv
-load_dotenv()
-
 from fastapi import FastAPI
+from app.amazon_client import obtener_datos_producto
+from dotenv import load_dotenv
+import os
+
+# Cargar variables de entorno
+load_dotenv()
 
 app = FastAPI()
 
 @app.get("/")
-def read_root():
-    return {"mensaje": "¡Todo funciona correctamente!"}
+def home():
+    return {"mensaje": "✅ API de Consejo Editorial activa"}
+
+@app.get("/producto/{asin}")
+def producto(asin: str):
+    resultado = obtener_datos_producto(asin)
+    if resultado:
+        return resultado
+    return {"error": "❌ No se pudo obtener el producto"}
